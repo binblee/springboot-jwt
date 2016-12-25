2582a38170afc292c212366b9ad8097d27bdbce9
Li Bin <binblee@users.noreply.github.com>
2016-12-25 22:44:37 +0800

    remove unused imports

--- src/main/java/com/example/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/security/jwt/JwtAuthenticationFilter.java
@@ -1,9 +1,7 @@
 package com.example.security.jwt;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
